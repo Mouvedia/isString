c320018dc884abff3b2a0b611551a291d89dc47a
Marc G <info@mouvedia.com>
2015-07-05 21:57:00 +0200

    prototype is spoofable

--- isString.max.js
+++ isString.max.js
@@ -8,34 +8,35 @@ String.isString = (function() {
         isString        = function(str) {
 // The try-catch-finally construct has performance impacts due to
 // the fact that it creates and destroys a variable at runtime.
-            try {
+// Furthermore IE didn't support it until version 5.
+        /*@cc_on
+          @if (@_jscript_version >= 5) @*/
+          try {
 // String.prototype.toString asserts that str's [[StringData]] internal slot
 // is a String value effectively guarding us against @@toStringTag spoofing.
 // cf http://www.ecma-international.org/ecma-262/5.1/#sec-15.5.4.2
-                strToString.call(str);
-                return true;
-            } catch (e) {
-                return false;
-            }
+            strToString.call(str);
+            return true;
+          } catch (e) {
+            return false;
+          }
+          /*@end
+        @*/
         };
 
     return function(str) {
 // take care of the most common case first
         return  typeof str === 'string' ||
-// optimized black magic
-                (str && typeof str === 'object' && (
+                (str && typeof str === 'object' &&
                 /*@cc_on
-                    @if (@_jscript_version >= 5) @*/
-                        str instanceof String ||
-                    /*@end
-
-                    @if (@_jscript_version < 5.5)
-                        /^\s*function\s*String\(\)\s*{\s*\[native code\]\s*}\s*$/.test(str.constructor)
-                    @else @*/
-                        hasToStringTag ? isString(str) : objToString.call(str) === '[object String]'
-                    /*@end
+// IE didn't support Function.prototype.call until version 5.5
+                  @if (@_jscript_version < 5.5)
+                    /^\s*function\s*String\(\)\s*{\s*\[native code\]\s*}\s*$/.test(str.constructor)
+                  @else @*/
+                    hasToStringTag ? isString(str) : objToString.call(str) === '[object String]'
+                  /*@end
                 @*/
 // fallback for falsy values
-                )) || false;
+                ) || false;
     };
 })();
