596ed8928675555aa0e210bc485f33efe2b36663
Marc G <info@mouvedia.com>
2015-07-18 04:11:28 +0200

    mitigate Function.prototype.call tampering

--- isString.js
+++ isString.js
@@ -1,12 +1,17 @@
+/*jshint expr:true*/
+
 String.isString = (function() {
     var objToString     = ({}).toString,
         strToString     = ('').toString,
+        hasBind         = Function.prototype.bind,
+        objToStrCall    = hasBind && objToString.call.bind(objToString),
+        strToStrCall    = hasBind && strToString.call.bind(strToString),
         hasToStringTag  = typeof Symbol === 'function' && typeof Symbol.toStringTag === 'symbol',
         isString        = function(str) {
         /*@cc_on
           @if (@_jscript_version >= 5) @*/
             try {
-                strToString.call(str);
+                hasBind ? strToStrCall(str) : strToString.call(str);
                 return true;
             } catch (e) {
                 return false;
@@ -22,7 +27,7 @@ String.isString = (function() {
                     @if (@_jscript_version < 5.5)
                         /^\s*function\s*String\(\)\s*{\s*\[native code\]\s*}\s*$/.test(str.constructor)
                     @else @*/
-                        hasToStringTag ? isString(str) : objToString.call(str) === '[object String]'
+                        hasToStringTag ? isString(str) : (objToStrCall(str) || objToString.call(str)) === '[object String]'
                     /*@end
                 @*/
                 ) || false;
